db40a0faac01be662d1d8b5dbabe29f44dd8085f
Ivan Nikolaev Ivanov <114014853+Ivano0ovIvan@users.noreply.github.com>
2023-02-18 09:57:21 +0200

    Add files via upload

---
+++ error_handling/repeat_text.py
@@ -0,0 +1,10 @@
+def repeat_text(text, number):
+    return text * number
+
+
+try:
+    text = input()
+    number = int(input())
+    print(repeat_text(text, number))
+except ValueError:
+    print("Variable times must be an integer")
